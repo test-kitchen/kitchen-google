f5087a08238b7490df1d98e336cc0ecf36ae6087
Andrew Leonard <andy@hurricane-ridge.com>
2013-10-20 07:10:06 -0700

    Initial implementation of Google Compute Engine driver.

---
+++ lib/kitchen/driver/gce.rb
@@ -0,0 +1,80 @@
+# Author:: Andrew Leonard (<andy@hurricane-ridge.com>)
+#
+# Copyright (C) 2013, Andrew Leonard
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+require 'fog'
+
+require 'kitchen'
+
+module Kitchen
+  module Driver
+    class Gce < Kitchen::Driver::SSHBase
+
+      default_config :machine_type,  'n1-standard-1'
+      default_config :name,          "test-kitchen-#{Time.now.to_i}"
+      default_config :username,      ENV['USER']
+      default_config :zone_name,     'us-central-1b'
+
+      required_config :google_client_email
+      required_config :google_key_location
+      required_config :google_project
+      required_config :image_name
+
+      def create(state)
+        server= create_instance
+        state[:server_id] = server.identity
+
+        info("GCE instance <#{state[:server_id]}> created.")
+        server.wait_for { print '.'; ready? } ; print '(server ready)'
+        state[:hostname] = server.public_ip_address || server.private_ip_address
+        wait_for_sshd(state[:hostname], config[:username])
+        puts '(ssh ready)'
+      rescue Fog::Errors::Error, Excon::Errors::Error => ex
+        raise ActionFailed, ex.message
+      end
+
+      def destroy(state)
+        return if state[:server_id].nil?
+
+        server = connection.servers.get(state[:server_id])
+        server.destroy unless server.nil?
+        info("GCE instance <#{state[:server_id]}> destroyed.")
+        state.delete(:server_id)
+        state.delete(:hostname)
+      end
+
+      private
+      
+      def connection
+        Fog::Compute.new({
+          :provider            => 'google',
+          :google_client_email => config[:google_client_email],
+          :google_key_location => config[:google_key_location],
+          :google_project      => config[:google_project],
+        })
+      end
+
+      def create_instance
+        connection.servers.create({
+          :name         => config[:name],
+          :image_name   => config[:image_name],
+          :machine_type => config[:machine_type],
+          :zone_name    => config[:zone_name],
+        })
+      end
+
+    end
+  end
+end
