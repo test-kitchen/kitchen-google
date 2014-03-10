b19e00d752b1924b1e9f341a216bab4194803447
Andrew Leonard <andy@hurricane-ridge.com>
2014-03-10 05:59:20 -0700

    Test #initialize defaults.

--- spec/kitchen/driver/gce_spec.rb
+++ spec/kitchen/driver/gce_spec.rb
@@ -11,11 +11,34 @@ describe Kitchen::Driver::Gce do
   end
 
   describe '#initialize' do
-    context 'default options' do
+    context 'with default options' do
       it 'defaults to the "us" area' do
         expect(driver[:area]).to eq('us')
       end
+
+      it 'defaults to an n1-standard-1 instance' do
+        expect(driver[:machine_type]).to eq('n1-standard-1')
+      end
+
+      it 'defaults to the "default" network' do
+        expect(driver[:network]).to eq('default')
+      end
+
+      it 'defaults to a nil instance name' do
+        expect(driver[:inst_name]).to be(nil)
+      end
+
+      it 'defaults to an empty array of tags' do
+        expect(driver[:tags]).to eq([])
+      end
+
+      it 'defaults to the running username' do
+        expect(driver[:username]).to eq(ENV['USER'])
+      end
+
+      it 'does not specify a zone' do
+        expect(driver[:zone_name]).to be(nil)
+      end
     end
   end
-
 end
