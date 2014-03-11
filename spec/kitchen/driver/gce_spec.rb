3cb5091da55d454a15b2dd4f9af7e730a7f149dd
Andrew Leonard <andy@hurricane-ridge.com>
2014-03-11 06:39:33 -0700

    More tests - #generate_name, and beginnings of #create.

--- spec/kitchen/driver/gce_spec.rb
+++ spec/kitchen/driver/gce_spec.rb
@@ -5,9 +5,16 @@ require_relative '../../spec_helper.rb'
 describe Kitchen::Driver::Gce do
 
   let(:config) { Hash.new }
+  let(:state) { Hash.new }
+
+  let(:instance) do
+    double(name: 'default-distro-12')
+  end
 
   let(:driver) do
-    Kitchen::Driver::Gce.new(config)
+    d = Kitchen::Driver::Gce.new(config)
+    d.instance = instance
+    d
   end
 
   describe '#initialize' do
@@ -49,4 +56,41 @@ describe Kitchen::Driver::Gce do
       end
     end
   end
+
+  describe '#create' do
+    context 'with an existing server' do
+      let(:state) do
+        s = Hash.new
+        s[:server_id] = 'default-distro-12345678'
+        s
+      end
+
+      it 'returns if server_id already exists' do
+        expect(driver.create(state)).to equal nil
+      end
+    end
+  end
+
+  describe '#generate_name' do
+    context 'with a name less than 28 characters' do
+      it 'concatenates the name and a UUID' do
+        expect(driver.send(:generate_name)).to match(
+          /^default-distro-12-[a-f0-9]{8}-([a-f0-9]{4}-){3}[a-f0-9]{12}$/)
+      end
+    end
+
+    context 'with a name 28 characters or longer' do
+      let(:instance) do
+        double(name: '1234567890123456789012345678')
+      end
+
+      it 'shortens the base name and appends a UUID' do
+        expect(driver.send(:generate_name)).to match(
+          /^123456789012345678901234567
+            -[a-f0-9]{8}-([a-f0-9]{4}-){3}[a-f0-9]{12}$/x)
+      end
+    end
+
+  end
+
 end
